a1a95ad54a7d169a8eed6209b18e81f2521ddffe
Mark Zhang <imziyang@gmail.com>
2016-01-12 18:03:02 +0800

    edit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,34 @@ var rename = require('gulp-rename');
 var del = require('del');
 
 var paths = {
+  cname: 'CNAME',
   html: 'index.html',
+  readme: 'README.md',
   scripts: ['js/*.js', '!js/templates.js'],
   styles: 'css/*.css',
   views: 'view/*.html',
   buildRoot: 'dist',
   buildScripts: 'dist/js',
   buildStyles: 'dist/css',
-  buildHtml: 'dist/index.html'
+  buildHtml: 'dist/index.html',
+  buildCname: 'dist/CNAME',
+  buildReadme: 'dist/README.md'
 };
 
 gulp.task('clean', function() {
-  return del([buildRoot]);
+  return del([paths.buildRoot]);
+});
+
+gulp.task('cname', ['clean'], function () {
+  return gulp.src(paths.cname)
+  .pipe(rename(paths.buildCname))
+  .pipe(gulp.dest('.'));
+});
+
+gulp.task('readme', ['clean'], function () {
+  return gulp.src(paths.readme)
+  .pipe(rename(paths.buildReadme))
+  .pipe(gulp.dest('.'));
 });
 
 gulp.task('temp', ['clean'], function() {
@@ -49,19 +65,19 @@ gulp.task('html', ['clean'], function() {
   var now = new Date().getTime();
   return gulp.src(paths.html)
     .pipe(htmlreplace({
-      'css': '/'+buildStyles+'/all.min.css?v='+now,
-      'js': ['/'+buildScripts+'/templates.js?v='+now, '/'+buildScripts+'/all.min.js?v='+now],
+      'css': '/css/all.min.css?v='+now,
+      'js': ['/js/templates.js?v='+now, '/js/all.min.js?v='+now],
       'weinre': ''
     }))
     .pipe(rename(paths.buildHtml))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['temp', 'cname', 'readme', 'scripts', 'styles', 'html'], function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({
       branch: 'master'
     }));
 });
 
-gulp.task('default', ['temp', 'scripts', 'styles', 'html', 'deploy']);
+gulp.task('default', ['temp', 'cname', 'readme', 'scripts', 'styles', 'html', 'deploy']);
